23564696bfeb48a6e268fc3547bcd0da6ed0c8c0
Berkan YILMAZ <55620672+berkanyilmz@users.noreply.github.com>
2021-07-21 15:21:01 +0300

    Update Main.java

--- EvenFibonacciNumbers/src/Main.java
+++ EvenFibonacciNumbers/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int sum = 2;
 //1 2 3 5 8 13 21 34
 //a b c
- // a b c
+//  a b c
         while (c < 4000000) {
             if (c % 2 == 0) {
                 sum += c;
